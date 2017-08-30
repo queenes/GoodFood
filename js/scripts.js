a7410534a1c922b6a9ba33b32d55fad0ba54951b
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-08-30 12:56:40 +0300

    1.Modify css to include styling for search button 2.Add js/scripts.js from dvt-pages 3.Modify search button in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var recipe1 = {
   name: "nelly",
   food: "kimchi-fried-rice",
   image:"assets/img/kimchi-fried-rice.jpg",
-  instructions: "happy coocking ",
+  instructions: "happy cooking ",
 
 };
 var recipe2 = {
@@ -16,7 +16,7 @@ var recipe3 = {
   name: "esther",
   food: "fluffy-pancakes",
   image:"img/",
-  instructions: "eat some sanck",
+  instructions: "eat some snack",
 };
 var recipe4 = {
   name: "granton",
